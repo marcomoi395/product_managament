78c2566512f066678a19d700ea2255b371a51b16
Thanh Loi <marcomoi395@gmail.com>
2024-05-05 10:25:24 +0700

    cấu hình .env

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+require("dotenv").config();
+
 const route = require("./routes/client/index.route");
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -10,5 +13,5 @@ app.set("view engine", "pug");
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
