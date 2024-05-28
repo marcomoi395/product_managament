21feae46d2b0d2f5336f6a6b6b74f70ca76bc87e
Thanh Loi <marcomoi395@gmail.com>
2024-05-28 14:20:09 +0700

    Update uploadCloud.middleware.js

--- middlewares/admin/uploadCloud.middleware.js
+++ middlewares/admin/uploadCloud.middleware.js
@@ -5,7 +5,7 @@ const streamifier = require('streamifier');
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    api_secret: process.env.API_SECRET
 });
 
 module.exports.upload = (req, res, next) => {
